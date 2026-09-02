78bd3cfb507e5f5fbc451302f55400344bb9d324
Jamie Sykes <jaymeh@users.noreply.github.com>
2026-09-02 12:29:04 +0100

    Switch to using base vite config to match other sites and utilise prettier instead of viteplus for code formatting.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite-plus';
+import { defineConfig } from 'vite';
 
 const ddevHostname =
     process.env.DDEV_HOSTNAME?.split(',')[0] ??
@@ -13,33 +13,6 @@ if (primaryUrl) {
 }
 
 export default defineConfig({
-    fmt: {
-        printWidth: 80,
-        tabWidth: 4,
-        useTabs: false,
-        semi: true,
-        singleQuote: true,
-        overrides: [
-            {
-                files: ['**/*.yml'],
-                options: {
-                    tabWidth: 2,
-                },
-            },
-        ],
-        sortImports: {
-            groups: [
-                'builtin',
-                'external',
-                'internal',
-                'parent',
-                'sibling',
-                'index',
-            ],
-            newlinesBetween: false,
-        },
-        ignorePatterns: ['resources/views/mail/*'],
-    },
     plugins: [
         laravel({
             input: ['resources/scss/app.scss', 'resources/js/app.js'],
